32f538ab14a92fe8eafdb807e4233be960e69b12
mario70pt <mario70pt@users.noreply.github.com>
2017-04-23 19:32:32 +0200

    komenty do skryptu

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,12 @@
 // plik scripts.js
 
 var name = prompt('Wpisz swoje imię');
+// to jest linia alertu
 alert('Witaj, ' + name);
+// to jest linia consoli
 console.log('Witaj, ' + name);
+
+/* 
+tu znajdziesz best practices a propos wyglądu i czytelności:
+https://github.com/airbnb/javascript
+*/
